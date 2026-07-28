a11a763467859cc46527acbd88158b869f031ca5
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2026-07-28 21:18:13 +0300

    Time: 285 ms (11.30%) | Memory: 21 MB (5.07%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -2,15 +2,28 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         n = len(names) 
 
-        for i in range(1, n):
-            index = i
-            
-            while index > 0 and heights[index] > heights[index - 1]:
-                heights[index], heights[index - 1] = heights[index - 1], heights[index]
-                names[index], names[index - 1] = names[index - 1], names[index]
-                index -= 1
+        count = [0 for i in range(10**5 + 1)] 
+        for h in heights:
+            count[h] += 1
         
-        return names
+        mapp = {heights[i]:names[i] for i in range(n)}
+        result = []
+        for i in range(10**5 + 1):
+            if count[i] == 1:
+                result.append(mapp[i])
+        
+        return result[::-1]
+
+
+        # for i in range(1, n):
+        #     index = i
+            
+        #     while index > 0 and heights[index] > heights[index - 1]:
+        #         heights[index], heights[index - 1] = heights[index - 1], heights[index]
+        #         names[index], names[index - 1] = names[index - 1], names[index]
+        #         index -= 1
+        
+        # return names
 
 
             
