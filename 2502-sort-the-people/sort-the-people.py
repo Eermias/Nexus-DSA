71056aabfb36bfd7ea795865cb5383650029795b
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2026-07-28 20:57:17 +0300

    Time: 595 ms (5.53%) | Memory: 19.3 MB (99.42%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,19 +1,16 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        n = len(names)
+        n = len(names) 
 
-        min_index = 0
-        while min_index < n:
-            max_index = min_index
-            for i in range(min_index, n):
-                if heights[i] > heights[max_index]:
-                    max_index = i
+        for i in range(1, n):
+            index = i
             
-            heights[min_index], heights[max_index] = heights[max_index], heights[min_index]
-            names[min_index], names[max_index] = names[max_index], names[min_index]
-        
-            min_index += 1
+            while index > 0 and heights[index] > heights[index - 1]:
+                heights[index], heights[index - 1] = heights[index - 1], heights[index]
+                names[index], names[index - 1] = names[index - 1], names[index]
+                index -= 1
         
         return names
 
-        
+
+            
