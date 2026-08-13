0b13d18e206c3bf45186d68087019633651be25a
Ermias Gedamu Abathun <101344704+Eermias@users.noreply.github.com>
2026-08-13 20:34:56 +0300

    Time: 315 ms (7.34%) | Memory: 19.8 MB (25.99%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+
+        count = defaultdict(int)
+        longest = 0
+        l, r = 0, 0
+        while r < len(s):
+            count[s[r]] += 1
+
+            # as long as there is a reapeating character ...
+            while count[s[r]] > 1:
+                count[s[l]] -= 1
+                l += 1
+            
+            # we have a valid window
+            longest = max(longest, r - l + 1)
+            r += 1
+        
+        return longest
+            
+            
+        
